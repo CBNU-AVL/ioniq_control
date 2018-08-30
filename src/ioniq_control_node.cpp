e07b86ce6d35411cbd8a98b6f72f15253f32d81c
Lee Jaemin <ljm6525@gmail.com>
2018-08-30 19:00:00 +0900

    loading path when initializing class

--- src/ioniq_control_node.cpp
+++ src/ioniq_control_node.cpp
@@ -1,14 +1,17 @@
+#include <vector>
+#include <fstream>
+#include <sstream>
 #include <boost/bind.hpp>
+
 #include <ros/ros.h>
 #include <tf/tf.h>
-#include <vector>
 
-#include <ublox_msgs/NavPVT.h>
-#include <ublox_msgs/NavRELPOSNED.h>
-#include <can_msgs/Frame.h>
 #include <geometry_msgs/PoseArray.h>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <ublox_msgs/NavPVT.h>
+#include <ublox_msgs/NavRELPOSNED.h>
+#include <can_msgs/Frame.h>
 
 class IoniqControlNode
 {
@@ -34,61 +37,97 @@ public:
     timerCmdCalc = nh.createTimer(ros::Duration(0.02), &IoniqControlNode::timerCallback, this);
 
     //load gps path
+    std::ifstream fin;
+    double pathLon, pathLat, pathAlt;
+    std::string input;
+    bool error;
+    unsigned long long int pathCount = 0;
+
+    fin.open("PATH.txt");
+    error=false;
+
+    while (true)
+      {
+        getline(fin,input);
+        if (!fin) break; //check for eof
+
+        std::istringstream buffer(input);
+        buffer >> pathCount >> pathLon >> pathLat >> pathAlt;
+
+        //check for non numerical input
+        //and less/more input than needed
+        if (!buffer || !buffer.eof())
+          {
+            error = true;
+            pathCount = 0;
+            break;
+          }
+
+        //do what you want with temp1 and temp2
+        ROS_DEBUG_STREAM("[" << pathCount << "] longitude: " << pathLon << '\t' << "latitude : " << pathLat );
+      }
+
+    if (error)
+      ROS_ERROR("path file is corrupted...");
   }
+
   void navPVTCallback(const ublox_msgs::NavPVT::ConstPtr &_msg)
   {
     if(_msg->flags & (ublox_msgs::NavPVT::CARRIER_PHASE_FLOAT | ublox_msgs::NavPVT::CARRIER_PHASE_FIXED))
-    {
-      currentVelocity.linear.x = _msg->velN * 0.001;
-      currentVelocity.linear.y = _msg->velE * 0.001;
-      currentVelocity.linear.z = _msg->velD * 0.001;
-      if(_msg->flags & ublox_msgs::NavPVT::FLAGS_HEAD_VEH_VALID)
       {
-        currentPose.pose.orientation.w = _msg->headVeh;
-      }
+        currentVelocity.linear.x = _msg->velN * 0.001;
+        currentVelocity.linear.y = _msg->velE * 0.001;
+        currentVelocity.linear.z = _msg->velD * 0.001;
+        if(_msg->flags & ublox_msgs::NavPVT::FLAGS_HEAD_VEH_VALID)
+          {
+            currentPose.pose.orientation.w = _msg->headVeh;
+          }
 
-    }
+      }
     ROS_WARN("ublox receiver is not rtk.");
   }
+
   void navRELPOSCallback(const ublox_msgs::NavRELPOSNED::ConstPtr &_msg)
   {
     if(_msg->flags & (ublox_msgs::NavRELPOSNED::FLAGS_CARR_SOLN_FLOAT | ublox_msgs::NavRELPOSNED::FLAGS_CARR_SOLN_FIXED))
-    {
-      //TODO: introduce tf for convenient transformation
-      currentPose.header.frame_id = "gps";
-      currentPose.header.stamp = ros::Time::now();
-      //FIXME: the frame 'gps' follows NED coordinate?
-      currentPose.pose.position.x = (_msg->relPosN + _msg->relPosHPN * 0.01) * 0.01;
-      currentPose.pose.position.y = (_msg->relPosE + _msg->relPosHPE * 0.01) * 0.01;
-      currentPose.pose.position.z = (_msg->relPosD + _msg->relPosHPD * 0.01) * 0.01;
-    }
+      {
+        //TODO: introduce tf for convenient transformation
+        currentPose.header.frame_id = "gps";
+        currentPose.header.stamp = ros::Time::now();
+        //FIXME: the frame 'gps' follows NED coordinate?
+        currentPose.pose.position.x = (_msg->relPosN + _msg->relPosHPN * 0.01) * 0.01;
+        currentPose.pose.position.y = (_msg->relPosE + _msg->relPosHPE * 0.01) * 0.01;
+        currentPose.pose.position.z = (_msg->relPosD + _msg->relPosHPD * 0.01) * 0.01;
+      }
   }
+
   void gcanCallback(const can_msgs::Frame::ConstPtr &_msg)
   {
     //TODO: define custom message for GCAN
     if(_msg->id == 0x710)
-    {
-      if((_msg->data[0] & 0x01) == 0)
       {
-        ROS_WARN("Steer control is not enabled.");
+        if((_msg->data[0] & 0x01) == 0)
+          {
+            ROS_WARN("Steer control is not enabled.");
+          }
       }
-    }
     if(_msg->id == 0x711)
-    {
-      ROS_INFO("  - R ID:%4x LEN:%1x DATA:%02x %02x %02x %02x %02x %02x %02x %02x\n",
-               _msg->id, _msg->dlc,
-               _msg->data[0], _msg->data[1],
-          _msg->data[2], _msg->data[3],
-          _msg->data[4], _msg->data[5],
-          _msg->data[6], _msg->data[7]);
-    }
+      {
+        ROS_INFO("  - R ID:%4x LEN:%1x DATA:%02x %02x %02x %02x %02x %02x %02x %02x\n",
+                 _msg->id, _msg->dlc,
+                 _msg->data[0], _msg->data[1],
+            _msg->data[2], _msg->data[3],
+            _msg->data[4], _msg->data[5],
+            _msg->data[6], _msg->data[7]);
+      }
   }
+
   void timerCallback(const ros::TimerEvent &_event)
   {
     if(!bActive)
-    {
-      return;
-    }
+      {
+        return;
+      }
     tf::Quaternion q(
           currentPose.pose.orientation.x,
           currentPose.pose.orientation.y,
@@ -100,41 +139,43 @@ public:
     const double k = 1;
     //TODO: Select appropriate target point to track: Nearest point(crossroad may cause problems)
     if (pathIndex <= 1 && pathIndex < path.poses.size())
-    {
-      steerAngle = yaw 
-          - atan2(path.poses[pathIndex].position.y - path.poses[pathIndex - 1].position.y, 
-          path.poses[pathIndex].position.x - path.poses[pathIndex - 1].position.x)
-          + atan2(k*distPose(path.poses[pathIndex], currentPose.pose), currentVelocity.linear.x);
+      {
+        steerAngle = yaw
+            - atan2(path.poses[pathIndex].position.y - path.poses[pathIndex - 1].position.y,
+            path.poses[pathIndex].position.x - path.poses[pathIndex - 1].position.x)
+            + atan2(k*distPose(path.poses[pathIndex], currentPose.pose), currentVelocity.linear.x);
 
-      can_msgs::Frame fMoConf;
-      fMoConf.id = 0x156;
-      fMoConf.dlc = 8;
-      fMoConf.data[0] = static_cast<unsigned char>(bEPSEnable | (bEPSIgnore << 2));
-      fMoConf.data[1] = static_cast<unsigned char>(steerSpeed * 2);
-      fMoConf.data[2] = static_cast<unsigned char>(bACCEnable | (bAEBEnable << 6));
-      fMoConf.data[3] = clusterDispSpeed;
-      fMoConf.data[7] = ++aliveCount;
+        can_msgs::Frame fMoConf;
+        fMoConf.id = 0x156;
+        fMoConf.dlc = 8;
+        fMoConf.data[0] = static_cast<unsigned char>(bEPSEnable | (bEPSIgnore << 2));
+        fMoConf.data[1] = static_cast<unsigned char>(steerSpeed * 2);
+        fMoConf.data[2] = static_cast<unsigned char>(bACCEnable | (bAEBEnable << 6));
+        fMoConf.data[3] = clusterDispSpeed;
+        fMoConf.data[7] = ++aliveCount;
 
-      can_msgs::Frame fMoVal;
-      fMoVal.id = 0x157;
-      fMoVal.dlc = 8;
-      uint16_t tmpSteerCmd = static_cast<uint16_t>(steerAngle * 10);
-      uint16_t tmpAccelCmd = static_cast<uint16_t>(vehAccel * 100 + 10.23);
-      uint8_t tmpAEBCmd = 1 * 100;
-      fMoVal.data[0] = static_cast<unsigned char>(tmpSteerCmd & 0x00FF);
-      fMoVal.data[1] = static_cast<unsigned char>((tmpSteerCmd & 0xFFFF) >> 8);
-      fMoVal.data[3] = static_cast<unsigned char>(tmpAccelCmd & 0x00FF);
-      fMoVal.data[4] = static_cast<unsigned char>((tmpAccelCmd & 0xFFFF) >> 8);
-      fMoVal.data[5] = tmpAEBCmd;
+        can_msgs::Frame fMoVal;
+        fMoVal.id = 0x157;
+        fMoVal.dlc = 8;
+        uint16_t tmpSteerCmd = static_cast<uint16_t>(steerAngle * 10);
+        uint16_t tmpAccelCmd = static_cast<uint16_t>(vehAccel * 100 + 10.23);
+        uint8_t tmpAEBCmd = 1 * 100;
+        fMoVal.data[0] = static_cast<unsigned char>(tmpSteerCmd & 0x00FF);
+        fMoVal.data[1] = static_cast<unsigned char>((tmpSteerCmd & 0xFFFF) >> 8);
+        fMoVal.data[3] = static_cast<unsigned char>(tmpAccelCmd & 0x00FF);
+        fMoVal.data[4] = static_cast<unsigned char>((tmpAccelCmd & 0xFFFF) >> 8);
+        fMoVal.data[5] = tmpAEBCmd;
 
-      pubCANCmd.publish(fMoConf);
-      pubCANCmd.publish(fMoVal);
-    }
+        pubCANCmd.publish(fMoConf);
+        pubCANCmd.publish(fMoVal);
+      }
   }
+
+  //utility functions
   static double distPose(const geometry_msgs::Pose &_a, const geometry_msgs::Pose &_b)
   {
-    return sqrt(pow(_a.position.x - _b.position.x, 2) 
-                + pow(_a.position.y - _b.position.y, 2) 
+    return sqrt(pow(_a.position.x - _b.position.x, 2)
+                + pow(_a.position.y - _b.position.y, 2)
                 + pow(_a.position.z - _b.position.z, 2));
   }
 
